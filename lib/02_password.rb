5831e42b6721f5a0bba496cc297c9596c2deae6a
iantsa raz <iantsaraz02@gmail.com>
2019-07-11 17:58:21 +0300

    ajout de fichier password

--- lib/02_password.rb
+++ lib/02_password.rb
@@ -1,23 +1,29 @@
-def ask_password
-	puts "entrez votre password"
+def sign_up
+	puts "sign up"
 	print ">"	
+	sign_up = gets.chomp
+end
+def login
+	puts "entrez votre password"
+	print ">"
 	password = gets.chomp
 	return password
+	
 end
-
 def condition(password)
-x="dada"
-while (x != password)
+    x="dada"
+    while (x != password)
 	puts "error"
 	puts "entrez votre password"
 	print ">"
 	password = gets.chomp  	
-end
+    end
 puts "password accepted"
 end
 
 def perform
-	password = ask_password
+	sign_up
+	password = login
 	condition(password)
 end
 
