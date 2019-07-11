8b7d2e8e1c38fc7b83fea0816745beb6f372f813
iantsa raz <iantsaraz02@gmail.com>
2019-07-11 16:19:01 +0300

    ajout du fichier wtf pyramid

--- lib/01_pyramids.rb
+++ lib/01_pyramids.rb
@@ -1,26 +1,33 @@
-def full_pyramide (number)
-	(number+1).times do |n|
-	print " "* (number - n)	
-	puts("#" *((2 *n+1)))
-   n =1
-   n+=1 
+def wtf_pyramid
+	puts "salut;bienvenu dans ma super pyramid; choisis un nombre"
+	print ">"
+	user_a= gets.to_i
 
+	if user_a% 2!= 0
+		user_a=(user_a+1)/2
+		user_b=user_a
+		a=0
+		base_pyramid =Array.new()
+		test= "*"
+		sect= " "
+while(a<user_a)
+		base_pyramid[a]=sect*(user_a)+test
+		puts"#{base_pyramid[a]}"
+		test = test+'*'+'*'
+		user_a -= 1
+end
+
+b=1
+test2 = "*"
+sect2 = " "
+while(b<user_b)
+		base_pyramid[b]= " "+ sect2+ test2*((user_b*2)-3)
+		puts"#{base_pyramid[b]}"
+		sect2 = sect2+ ' '
+		user_b -= 1
+end
+else
+	puts "vas te faire bouler"
 	end
 end
-
-def ask_number
-	puts"salut;bienvenue dans ma super pyramide! Combien d'etages veux-tu?"
-	puts"choisis un nombre entre 1 et 25"
-	print">"
-
-	number=gets.chomp.to_i
-	
-	return number
-end
-
-def perform
-	number = ask_number
-	full_pyramide (number)
-end
-
-perform
+wtf_pyramid()
